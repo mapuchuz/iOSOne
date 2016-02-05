aa5fae508fce4bc0ad44b7b19b1a3b5e1a1f3c96
vdemolombe <vdemolombe@imac-de-vdemolombe.local>
2016-02-05 14:34:01 -0800

    Initial Commit

---
+++ ConnexionServeur/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConnexionServeur
+//
+//  Created by vdemolombe on 05/02/2016.
+//  Copyright © 2016 vdemolombe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConnexionServeur/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ConnexionServeur
+//
+//  Created by vdemolombe on 05/02/2016.
+//  Copyright © 2016 vdemolombe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
